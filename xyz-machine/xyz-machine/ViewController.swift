6f5f93295cbf8e88181d97fde648b079b4986e55
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-01-28 05:29:24 -0600

    initial commit - project creation

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Nicholas Coggin on 1/28/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
